dfef7cce9a15cf93ffa73ea6e4c0d5b15aeac53c
obay.developer <obay.developer@gmail.com>
2024-08-23 19:56:42 +0000

    Fixed

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Analytics>
-      <body className={inter.className}>{children}</body>
-      </Analytics>
+      <body className={inter.className}>{children}
+      <Analytics/>
+      </body>
     </html>
   );
 }
